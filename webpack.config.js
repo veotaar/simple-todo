e37b9bc6497aff8bb1e619ea53672f81e890357f
Yalçıncan Ulus <yalcincanulus@gmail.com>
2022-06-21 01:40:12 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
+// const WorkboxPlugin = require("workbox-webpack-plugin");
+const isProduction = process.env.NODE_ENV === "production";
+
+const config = {
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  watchOptions: {
+    ignored: /node_modules/,
+    aggregateTimeout: 600,
+  },
+  devServer: {
+    static: "./dist",
+    open: true,
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
