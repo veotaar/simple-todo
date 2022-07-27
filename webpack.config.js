ecb2702f7887d57758cec53aa30dab13e467c877
Yalçıncan Ulus <yalcincanulus@gmail.com>
2022-07-27 12:49:23 +0300

    Add PWA support

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 const isProduction = process.env.NODE_ENV === "production";
 
 const config = {
@@ -24,6 +26,32 @@ const config = {
       template: "index.html",
     }),
     new FaviconsWebpackPlugin("./src/assets/favicon/logo.png"),
+    new WebpackPwaManifest({
+      name: "Simple Todo",
+      short_name: "ToDo",
+      description: "A to-do list application",
+      background_color: "#ffffff",
+      theme_color: "#0f172a",
+      publicPath: "./",
+      crossorigin: null, //can be null, use-credentials or anonymous
+      inject: true,
+      ios: true,
+      icons: [
+        {
+          src: path.resolve(__dirname, "./src/assets/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+        },
+        {
+          src: path.resolve(__dirname, "./src/assets/icon.png"),
+          size: "512x512",
+          purpose: "maskable",
+        },
+      ],
+    }),
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
   ],
   module: {
     rules: [
